c4b6f2bafd48455e636d3fc5aeda6834df81ac33
sasidharyadavalli <sasidharsasi218@gmail.com>
2024-12-24 22:24:45 +0530

    added heading in index.html added paragraph to index2.html,added car.java to learning2 dir

---
+++ Car.java
@@ -0,0 +1,9 @@
+package com.example.gitTutorial.Git.learnings2;
+
+public class Car {
+    public static void main(String args[]){
+        for(int i=1;i<=10;i++){
+            System.out.println( "2 * "+i+" = "+i*2);
+        }
+    }
+}
